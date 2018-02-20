ba148814d0491d8fe061292c502ad29bb6023416
Nikilesh Chilkuru <nikhilesharthik7@gmail.com>
2018-02-19 19:35:58 -0500

    updated travis.yml

---
+++ src/test/java/fetchbooks/springbootgraphql/FirstTest.java
@@ -0,0 +1,15 @@
+package fetchbooks.springbootgraphql;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.Test;
+
+public class FirstTest {
+
+	@Test
+	public void test() {
+		assertThat("Test Works")
+	      .isEqualTo("Test Works");
+	}
+
+}
